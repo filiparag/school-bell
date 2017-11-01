48e860e94685e89cc73b6e68c5a27187c623abaf
Filip Parag <filiparag.com@gmail.com>
2017-11-01 18:06:27 +0100

    Config

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ directory = path.dirname(path.realpath(__file__)) + sep
 
 config = None
 
+
 def get(section, key):
 
     global config
     if config is None:
         load()
-
     if config.get(section) is None:
         return None
     else:
@@ -23,39 +23,30 @@ def set(section, key, value):
     global config
     if config is None:
         load()
-
     if config.get(section) is None:
         config[section] = {}
-
     config[section][key] = value
+
 
 def verbose(message):
 
     timestamp = datetime.now().strftime("[ %Y/%m/%d %H:%M:%S ]")
-
     if get('bell', 'verbose'):
         print(timestamp + ' ' + message)
-
     if get('bell', 'log'):
         with open('log.txt', 'a') as log_file:
             log_file.write(timestamp + ' ' + message + '\n')
 
 
-
 def load():
-
+    
     parser = configparser.ConfigParser()
     parser.read(directory + 'config.ini')
-
     global config
     config = {}
-
     for section in parser.sections():
-
         section_config = {}
-
         for key in parser._sections[section]:
             value = eval(parser._sections[section][key])
             section_config[key] = value
-
         config[section] = section_config
