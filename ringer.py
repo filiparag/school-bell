87a288847cf798b6411996431bfeace5f8c57428
Filip Parag <filiparag.com@gmail.com>
2017-11-01 18:06:53 +0100

    Ringer

--- ringer.py
+++ ringer.py
@@ -5,32 +5,20 @@ from os import sep
 from subprocess import Popen
 
 
-def start(schedule):
+def start(schedule, active):
 
-    while not schedule.empty():
-
+    while not schedule.empty() and active():
         next_time = schedule.get()
-
         config.verbose('Next bell is at %s:%s:%s' %
                        (next_time[0], next_time[1], next_time[2]))
-
         while next_time > current_time():
             time.sleep(0.1)
-
-        if config.get('raspberry', 'gpio'):
-
-            import raspberry
-
-            if raspberry.active():
-                ring(next_time[3])
-            else:
-                config.verbose('Skipped bell at %s:%s:%s' %
-                               (next_time[0], next_time[1], next_time[2]))
-
-        else:
-
-            ring(next_time[3])
-
+            if not active():
+                config.verbose('Terminating ringer')
+                return
+        ring(next_time[3])
+    if not active():
+        config.verbose('Terminating ringer')
     return
 
 
@@ -38,13 +26,12 @@ def ring(sound_file):
 
     sound_file_target = config.directory + 'sounds' + sep + sound_file
     Popen([config.get('bell', 'player'), sound_file_target])
-
-    config.verbose('Playing %s with %s' % (sound_file_target, config.get('bell', 'player')))
+    config.verbose('Playing %s with %s' %
+                   (sound_file_target, config.get('bell', 'player')))
 
 
 def current_time():
 
     offset_time = datetime.datetime.now()\
-        + datetime.timedelta(seconds=config.get('bell', 'offset'))
-
+        + datetime.timedelta(seconds=config.get('bell', 'offset') - 1)
     return (offset_time.hour, offset_time.minute, offset_time.second)
