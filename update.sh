ad887dea81c9fda4a5f14e7dfabeeefb6007c041
Filip Parag <filiparag.com@gmail.com>
2017-11-02 16:04:56 +0100

    Updater

--- update.sh
+++ update.sh
@@ -3,6 +3,28 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
-git reset --hard
-git pull
-systemctl restart school-bell.service
+
+GIT_STATUS=`git pull`
+
+if [ "$GIT_STATUS" != "Already up-to-date." ]
+then
+
+    echo "Update available, fetching from GitHub..."
+
+    git reset --hard > /dev/null
+    git pull >> $DIR/updates.txt
+
+    echo "Update applied successfully!"
+
+    SERVICE_RUNNING=`systemctl is-active school-bell >/dev/null 2>&1 && echo YES || echo NO`
+    if [ "$SERVICE_RUNNING" == "YES" ]
+    then
+        echo "Restarting systemd service."
+        systemctl restart school-bell.service
+    fi
+
+else
+
+    echo "Already at the latest version!"
+
+fi
