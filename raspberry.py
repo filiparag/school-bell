ea3e8bcbfd121893959f4f79bb2a61d8def90174
Filip Parag <filiparag.com@gmail.com>
2018-01-14 01:35:10 +0100

    Complete overhaul [not yet tested]

--- raspberry.py
+++ raspberry.py
@@ -1,197 +1,318 @@
 import config
+import schedule
+import ringer
 import pickle
-import macro
-import time
 import os
 import threading
-import RPi.GPIO as GPIO
+import time
+import datetime
+# import RPi.GPIO as GPIO
 
-button_state = [False, False, False]
+state = None
 
+gpio = {
+    'led': {
+        'power': config.get('raspberry', 'led_power'),
+        'sched': config.get('raspberry', 'led_sched'),
+        'satur': config.get('raspberry', 'led_satur')
+    },
+    'button': {
+        'power': config.get('raspberry', 'btn_power'),
+        'sched': config.get('raspberry', 'btn_sched'),
+        'satur': config.get('raspberry', 'btn_satur'),
+        'mring': config.get('raspberry', 'btn_mring')
+    },
+    'check': config.get('raspberry', 'pin_check'),
+    'button_state': [False] * 4,
+    'connected_state': False
+}
 
-def configure(schedule_list):
-
-    global state
-    config.verbose('Loading state')
-    load_state()
-    state['schedule']['names'] = schedule_list
-    schedule_list.sort()
-
-    config.verbose('Selected schedule is \'%s\'' % schedule())
-
-    config.verbose('Configuring Raspberry PI')
-    GPIO.setwarnings(False)
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(config.get('raspberry', 'led_power'), GPIO.OUT)
-    GPIO.output(config.get('raspberry', 'led_power'), state['active'])
-    for led_pin in config.get('raspberry', 'led_array'):
-        GPIO.setup(led_pin, GPIO.OUT)
-    set_led_array(state['led'])
-
-    GPIO.setup(config.get('raspberry', 'btn_power'), GPIO.IN)
-    GPIO.setup(config.get('raspberry', 'btn_sched'), GPIO.IN)
-    GPIO.setup(config.get('raspberry', 'btn_macro'), GPIO.IN)
-
-    button_reader = threading.Thread(target=button_read, daemon=True)
-    button_reader.run()
-
-    config.verbose('GPIO is active')
-
-    config.verbose('Executing macro configure.')
-    macro.configure(set_led_array, config.verbose,)
-    save_state()
+gpio_thread = threading.Thread()
+bell_thread = threading.Thread()
 
 
-def button_read():
+def gpio_setup():
 
-    global button_state
+    config.verbose('Setting up GPIO pins')
 
-    while True:
-        button_state_new_1 = [not GPIO.input(config.get('raspberry', 'btn_power')),
-                              not GPIO.input(config.get(
-                                  'raspberry', 'btn_sched')),
-                              not GPIO.input(config.get('raspberry', 'btn_macro'))]
+    # GPIO.setwarnings(False)
+    # GPIO.cleanup()
+    # GPIO.setmode(GPIO.BCM)
+
+    # GPIO.setup(gpio['led']['power'], GPIO.OUT)
+    # for pin in gpio['led']['sched']:
+    #     GPIO.setup(pin, GPIO.OUT)
+    # GPIO.setup(gpio['led']['satur'], GPIO.OUT)
+
+    # for button in gpio['button']:
+    #     GPIO.setup(gpio[buttFon], GPIO.OUT)
+
+
+def gpio_cycle(active):
+
+    while active:
+
+        if gpio_connected():
+
+            # gpio_led()
+            # gpio_poll()
+
+            pass
+
+            # temp
         time.sleep(0.1)
-        button_state_new_2 = [not GPIO.input(config.get('raspberry', 'btn_power')),
-                              not GPIO.input(config.get(
-                                  'raspberry', 'btn_sched')),
-                              not GPIO.input(config.get('raspberry', 'btn_macro'))]
-
-        button_state_new = [one if one is two else False for one, two in
-                            zip(button_state_new_1, button_state_new_2)]
-
-        button_state_delta = [True if old is not new else False for old, new in
-                              zip(button_state, button_state_new)]
-
-        button_state = button_state_new
-
-        if button_state_delta != [False] * 3:
-            if button_state[0]:
-                action_active()
-            if button_state[1]:
-                action_schedule()
-            if button_state[2]:
-                action_macro()
 
 
-def active():
+def gpio_connected():
 
-    return state['active']
-
-
-def schedule():
-
-    return state['schedule']['names'][state['schedule']['active']]
+    return True
 
 
-def action_active():
+def gpio_poll():
 
-    global state
-    state['active'] = not state['active']
-    config.verbose('Power button pressed, turning %s.' %
-                   ('on' if state['active'] else 'off'))
-    if state['active']:
-        set_led_array(values=(False, False, False))
-    else:
-        set_led_array(number=state['schedule']['active'] + 1)
-    GPIO.output(config.get('raspberry', 'led_power'), state['active'])
-    config.verbose('Executing macro configure.')
-    macro.configure(set_led_array, config.verbose,)
-    save_state()
+    print('gpio_poll()')
 
+    # global button_state
 
-def action_schedule():
+    # while True:
+    #     button_state_new_1 = [not GPIO.input(config.get('raspberry', 'btn_power')),
+    #                           not GPIO.input(config.get(
+    #                               'raspberry', 'btn_sched')),
+    #                           not GPIO.input(config.get('raspberry', 'btn_macro'))]
+    #     time.sleep(0.1)
+    #     button_state_new_2 = [not GPIO.input(config.get('raspberry', 'btn_power')),
+    #                           not GPIO.input(config.get(
+    #                               'raspberry', 'btn_sched')),
+    #                           not GPIO.input(config.get('raspberry', 'btn_macro'))]
 
-    global state
-    if state['active']:
-        config.verbose('Schedule button pressed, ignoring.')
-        blink(config.get('raspberry', 'led_power'), 0.1, 2)
-    else:
-        state['schedule']['active'] = (state['schedule']['active'] + 1)\
-            % len(state['schedule']['names'])
-        config.verbose(
-            'Schedule button pressed, changing active schedule to \'%s\'.' % schedule())
-        set_led_array(number=state['schedule']['active'] + 1)
-    save_state()
+    #     button_state_new = [one if one is two else False for one, two in
+    #                         zip(button_state_new_1, button_state_new_2)]
 
+    #     button_state_delta = [True if old is not new else False for old, new in
+    #                           zip(button_state, button_state_new)]
 
-def action_macro():
+    #     button_state = button_state_new
 
-    config.verbose('Macro button pressed, executing macro.')
-    macro.button(set_led_array, config.verbose,)
-    save_state()
+    #     if button_state_delta != [False] * 3:
+    #         if button_state[0]:
+    #             action_active()
+    #         if button_state[1]:
+    #             action_schedule()
+    #         if button_state[2]:
+    #             action_macro()
 
 
-def load_state():
+def gpio_led():
 
-    use_default = True
-    global state
-    if os.path.isfile(config.directory + '.gpio_state'):
-        try:
-            with open(config.directory + '.gpio_state', 'rb') as pickle_file:
-                state = pickle.load(pickle_file)
-            use_default = False
-        except Exception:
-            config.verbose(
-                'Previous GPIO state could not be restored, using defaults')
+    print('gpio_led()')
 
-    if use_default:
-        config.verbose('No GPIO state file found, using defaults')
-        state = {
-            'active': True,
-            'led': (False, False, False),
-            'schedule': {
-                'active': 0,
-                'names': ['default']
-            },
-            'macro': None
+    # GPIO.output(gpio['led']['power'], state._led['power'])
+    # GPIO.output(gpio['led']['satur'], state._led['satur'])
+
+    # sched = 'ssche' if datetime.datetime.now().weekday() is 5 else 'sched'
+    # for pin in range(gpio['led']['sched']):
+    #     GPIO.output(gpio['led']['sched'][pin], state._led[sched][pin])
+
+
+def gpio_button_power():
+
+    state.active(not state._active)
+    config.verbose('Power button pressed!')
+
+
+def gpio_button_schedule():
+
+    state.schedule(next=True, saturday=state._saturday)
+    config.verbose('Schedule button pressed!')
+
+
+def gpio_button_saturday():
+
+    state.saturday(not state._saturday)
+    config.verbose('Saturday button pressed!')
+
+
+def gpio_button_ring():
+
+    config.verbose('Ring button pressed!')
+
+
+class State:
+
+    def __init__(self):
+
+        self._active = None
+        self._schedule = None
+        self._saturday = None
+        self._schedule_saturday = None
+
+        self._led = {
+            'power': False,
+            'sched': (0, 0, 0),
+            'ssche': (0, 0, 0),
+            'satur': False
         }
 
+        if not self.load():
 
-def save_state():
+            self.active(False)
+            self.saturday(False)
+            self.schedule(name=config.get('bell', 'schedule'))
+            self.schedule(name=config.get(
+                'raspberry', 'sch_satur'), saturday=True)
+
+            self.save()
+
+            config.verbose('Using default GPIO state values')
+
+    def active(self, value=None):
+
+        if value is not None:
+            self._active = value
+        else:
+            self._active = not self._active
+        self.led('active', value)
+
+        self.save()
+
+        config.verbose('School bell is now %sactive' % ('' if value else 'in'))
+
+    def schedule(self, name=None, next=False, saturday=False):
+
+        if name is not None and next is True:
+            raise ValueError('Schedule is double defined')
+
+        schedule_list = schedule.list_names()
+        schedule_list.sort()
+
+        if name is not None:
+            if saturday:
+                self._schedule_saturday = name
+                self.led(name='ssche', value=schedule_list.index(name))
+                config.verbose(
+                    'Current Saturday schedule is set to \'%s\'' % self._schedule_saturday)
+            else:
+                self._schedule = name
+                self.led(name='sched', value=schedule_list.index(name))
+                config.verbose('Current schedule is set to \'%s\'' %
+                               self._schedule)
+
+        if next is True:
+            if saturday:
+                current = schedule_list.index(self._schedule_saturday)
+                self._schedule_saturday = schedule_list[(
+                    current + 1) % len(schedule_list)]
+                config.verbose(
+                    'Current Saturday schedule is set to \'%s\'' % self._schedule_saturday)
+            else:
+                current = schedule_list.index(self._schedule)
+                self._schedule = schedule_list[(
+                    current + 1) % len(schedule_list)]
+                config.verbose('Current schedule is set to \'%s\'' %
+                               self._schedule)
+
+    def saturday(self, value):
+
+        if value is not None:
+            self._saturday = value
+        else:
+            self._saturday = not self._saturday
+        self.led('satur', value)
+
+        self.save()
+
+        config.verbose('School bell is now %sactive on Saturdays' %
+                       ('' if value else 'in'))
+
+    def led(self, name=None, value=False):
+
+        if name is not None:
+            if name is 'sched' or name is 'ssche':
+                array = [False] * 3
+                if value >= 1 and value <= 3:
+                    array[value - 1] = True
+                else:
+                    array = [True] * 3
+                self._led[name] = array
+            else:
+                self._led[name] = value
+
+        self.save()
+
+    def load(self):
+
+        if os.path.isfile(config.directory + '.gpio_state'):
+            try:
+                with open(config.directory + '.gpio_state', 'rb') as pickle_file:
+                    loaded_state = pickle.load(pickle_file)
+                    self._active = loaded_state._active
+                    self._schedule = loaded_state._schedule
+                    self._saturday = loaded_state._saturday
+                    self._schedule_saturday = loaded_state._schedule_saturday
+                    self._led = loaded_state._led
+                    config.verbose('Loaded previous GPIO state')
+                    return True
+            except Exception:
+                config.verbose('Previous GPIO state could not be restored')
+        else:
+            return False
+
+    def save(self):
+
+        if config.get('raspberry', 'save'):
+            with open(config.directory + '.gpio_state', 'wb') as pickle_file:
+                pickle.dump(self, pickle_file)
+
+
+def initialize():
 
     global state
-    with open(config.directory + '.gpio_state', 'wb') as pickle_file:
-        pickle.dump(state, pickle_file)
+    state = State()
+    gpio_setup()
 
 
-def set_led_array(values=(False, False, False), number=None):
+def start():
 
     global state
-    if number is not None:
-        if number < 1:
-            values = (False, False, False)
-        elif number == 1:
-            values = (True, False, False)
-        elif number == 2:
-            values = (False, True, False)
-        elif number == 3:
-            values = (False, False, True)
-        elif number > 3:
-            values = (True, True, True)
-    state['led'] = values
-    for (led_pin, value) in zip(config.get('raspberry', 'led_array'), values):
-        GPIO.output(led_pin, value)
-    save_state()
+    if state is None:
+        raise ValueError('GPIO is not initialized')
+
+    config.verbose('Raspberry Pi mode is active')
+
+    global gpio_thread
+    gpio_thread = threading.Thread(
+        target=gpio_cycle, args=(True,), daemon=True)
+    gpio_thread.start()
+
+    while True:
+
+        if state._active:
+            if not bell_thread.is_alive():
+                today = datetime.datetime.now().weekday()
+                schedule_queue = None
+                if state._saturday and today == 5:
+                    schedule_queue = schedule.today(state._schedule_saturday)
+                else:
+                    schedule_queue = schedule.today(state._schedule)
+                import calendar
+                if not schedule_queue.empty():
+                    config.verbose('Started bell with the schedule for %s' %
+                                   calendar.day_name[today])
+                    bell_start(schedule_queue)
+
+        time.sleep(.5)
 
 
-def blink(pin, interval, repetitions):
+def bell_start(schedule_queue):
 
-    from multiprocessing import Process
-    blinking = Process(target=blinker, args=(pin, interval, repetitions,))
-    blinking.daemon = True
-    blinking.start()
+    global bell_thread
+    if bell_active():
+        bell_thread = threading.Thread(
+            target=ringer.start, args=(schedule_queue, bell_active,))
+        bell_thread.start()
 
 
-def blinker(pin, interval, repetitions):
+def bell_active():
 
-    import time
-    counter = 0
-    while counter < repetitions:
-        GPIO.output(pin, not GPIO.input(pin))
-        time.sleep(interval / 2)
-        GPIO.output(pin, not GPIO.input(pin))
-        time.sleep(interval / 2)
-        counter += 1
+    global state
+    return state._active
