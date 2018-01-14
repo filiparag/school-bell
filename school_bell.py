bd4d78865a96ae8de6b70d713a69dbd85d9a9d07
Filip Parag <filiparag.com@gmail.com>
2018-01-14 01:34:31 +0100

    New Raspberry Pi mode

--- school_bell.py
+++ school_bell.py
@@ -11,17 +11,8 @@ def main():
     if config.get('raspberry', 'gpio'):
         config.verbose('Running in Raspberry Pi mode')
         import raspberry
-        import time
-        raspberry.configure(schedule.list_names())
-        while True:
-            if raspberry.active():
-                schedule_name = raspberry.schedule()
-                schedule_queue = schedule.today(schedule_name)
-                if not schedule_queue.empty():
-                    config.verbose('Schedule: %s' % schedule_name)
-                    ringer.start(schedule_queue, raspberry.active)
-                schedule.wait_tomorrow(lambda: not raspberry.active())
-            time.sleep(0.1)
+        raspberry.initialize()
+        raspberry.start()
     else:
         config.verbose('Running in regular mode')
         while True:
